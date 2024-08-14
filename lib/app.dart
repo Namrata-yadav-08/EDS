3fcc83ebc193c38fe3bab64f343daffd444bd4e8
Vedanshi27vishu <vedanshi27vishu@gmail.com>
2024-08-15 02:35:06 +0530

    First commit

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:zoom/screens/login.dart';
+import 'package:zoom/utils/colors.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zoom clone',
+      home: const LoginScreen(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+    );
+  }
+}
