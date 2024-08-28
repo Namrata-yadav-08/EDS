a330bf45c3a5d77c4283981468168e9b3d483773
Aikansh Tiwari <132533740+aikansh008@users.noreply.github.com>
2024-08-29 01:59:28 +0530

    Resolved conflicts

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:zoom/screens/login.dart';
 import 'package:zoom/utils/colors.dart';
+import 'package:zoom/screens/bottomnav.dart';
+import 'package:zoom/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:io' show Platform;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
